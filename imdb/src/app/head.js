cdd6c41b7244bcecae183bd8d7f8159c00b1cf7c
ivanzhang <ivanzhang220820@gmail>
2023-02-25 14:26:54 -0500

    imdb project with next.js

---
+++ imdb/src/app/head.js
@@ -0,0 +1,11 @@
+export default function head() {
+  return (
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="An imdb clone website" />
+      <link rel="icon" href="/favicon.ico" />
+      <title>Create Next App</title>
+    </head>
+  )
+}
