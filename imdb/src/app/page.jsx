37bbc854c3dcdd3c7364bb63b7d114859248f837
ivanzhang <ivanzhang220820@gmail>
2023-03-03 00:05:55 -0500

    Add fetch data from TMDB

--- imdb/src/app/page.jsx
+++ imdb/src/app/page.jsx
@@ -1,12 +1,22 @@
+import Results from '../components/Results';
 
-import { Inter } from 'next/font/google'
+const API_KEY = process.env.TMDB_API_KEY
 
-const inter = Inter({ subsets: ['latin'] })
+export default async function Home({searchParams}) {
+    const genre = searchParams.genre || 'fetchTrendingData';
+    const res = await fetch(
+        `https://api.themoviedb.org/3/${
+            genre === "fetchTopRatedData"
+            ? "movie/top_rated"
+            : "trending/all/week"
+         }?api_key=${API_KEY}&language=en-US&page=1`,{ next: { revalidate: 50000 } });
+    if (!res.ok) {
+        throw new Error(res.statusText);
+    }
+    const data = await res.json();
+    const results = data.results;
+   // console.log(results);
+    return <div><Results results={results}></Results></div>
 
-export default function Home() {
-     return (
-         <h1
-             className='text-red-700'
-         >Home Page</h1>
-  )
+
 }
