fcb0f117166abe2d97d20779e2ba7a18934042c3
ivanzhang <ivanzhang220820@gmail>
2023-03-03 22:11:41 -0500

    Add loading.jsx and error.jsx

---
+++ imdb/src/components/Card.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Card({ result }) {
+    return (
+        <div className="cursor-pointer
+        sm:p-3 sm:hover:shadow-slate-400
+        sm:shadow-md rounded-lg
+        sm:border sm:border-slate-400
+        sm:m-2 transition-shadow duration-200 group ">
+            <Link href={`/movie/${ result.id }`}>
+                <Image src={`https://image.tmdb.org/t/p/original/${
+                    result.backdrop_path || result.poster_path
+                }`} width={500} height={300}
+                    className="sm:rounded-t-lg group-hover:opacity-70 duration-200"
+                    placeholder="blur"
+                    blurDataURL="/spinner.svg"
+                    alt="image is not available">
+
+                </Image>
+            </Link>
+        </div>
+    )
+}
