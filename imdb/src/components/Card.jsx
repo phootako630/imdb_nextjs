c2136f1193691593f458f5fcc6063fd751f94060
ivanzhang <ivanzhang220820@gmail>
2023-03-05 00:12:38 -0500

    Add card for each movie

--- imdb/src/components/Card.jsx
+++ imdb/src/components/Card.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import {FaRegThumbsUp} from "react-icons/fa";
 
 export default function Card({ result }) {
     return (
@@ -12,12 +13,20 @@ export default function Card({ result }) {
                 <Image src={`https://image.tmdb.org/t/p/original/${
                     result.backdrop_path || result.poster_path
                 }`} width={500} height={300}
+                    style={{maxWidth: "100%", height: "auto"}}
                     className="sm:rounded-t-lg group-hover:opacity-70 duration-200"
                     placeholder="blur"
                     blurDataURL="/spinner.svg"
                     alt="image is not available">
-
                 </Image>
+                <div className="p-2">
+                    <p className="line-clamp-2 text-base">{result.overview}</p>
+                    <h2 className="truncate text-lg font-bold">{result.title || result.name}</h2>
+                    <p className="flex items-center">
+                        {result.release_date || result.first_air_date}
+                        <FaRegThumbsUp className="h-5 mr-1 ml-3 text-blue-500"/>{result.vote_count}
+                    </p>
+                </div>
             </Link>
         </div>
     )
